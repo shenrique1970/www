655961239045c1f3ce330435b58915c15cd99828
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-13 15:38:20 -0300

    null

--- curso/tipos_primitivos/index.php
+++ curso/tipos_primitivos/index.php
@@ -7,6 +7,7 @@
     <title>Tipos primitivos</title>
     <link rel="stylesheet" href="css/estilo.css">
 </head>
+
 <body>
     <!--
     escalares
@@ -20,47 +21,61 @@
     <div id="escalares" class="tab active">
         <?php
 
-            echo '<h1><b>Tipos primitivos escalares</b></h1>';
+        echo '<h1><b>Tipos primitivos escalares</b></h1>';
 
-            echo '<h3>Inteiro - <code>$idade = 25 </code></h3>';
-            echo "do tipo "; $idade = 25; var_dump($idade);
+        echo '<h3>Inteiro - <code>$idade = 25 </code></h3>';
+        echo "do tipo ";
+        $idade = 25;
+        var_dump($idade);
 
-            echo '<h3>float/double - <code>$preco = 99.90 </code></h3>';
-            echo "do tipo "; $preco = 99.90; var_dump($preco);
+        echo '<h3>float/double - <code>$preco = 99.90 </code></h3>';
+        echo "do tipo ";
+        $preco = 99.90;
+        var_dump($preco);
 
-            echo '<h3>real a partir da versão 7 deixou de ser usasdo - <code>$preco = 99.90;</code></h3>';
-            
-            echo '<h3>string - <code>$mensagem = "Bem-vindo!" </code></h3>';
-            echo "do tipo "; $mensagem = "Bem-vindo!"; var_dump($mensagem);
+        echo '<h3>real a partir da versão 7 deixou de ser usasdo - <code>$preco = 99.90;</code></h3>';
 
-            echo '<h3>boolean - <code>$ativo = true </code></h3>';
-            echo "do tipo "; $ativo = true; var_dump($ativo);
+        echo '<h3>string - <code>$mensagem = "Bem-vindo!" </code></h3>';
+        echo "do tipo ";
+        $mensagem = "Bem-vindo!";
+        var_dump($mensagem);
 
-            echo '<h3>null - <code>$usuario = null </code></h3>';
-            echo "do tipo "; $usuario = null; var_dump($usuario);
+        echo '<h3>boolean - <code>$ativo = true </code></h3>';
+        echo "do tipo ";
+        $ativo = true;
+        var_dump($ativo);
 
-            echo '<h3>$num = 0x1A </h3>';
-            echo " do tipo "; $num = 0x1A; var_dump($num);
-            echo "<br>"; 
-            print "E o valor desta variável é $num";
+        echo '<h3>$num = 0x1A </h3>';
+        echo " do tipo ";
+        $num = 0x1A;
+        var_dump($num);
+        echo "<br>";
+        print "E o valor desta variável é $num";
 
-            echo '<h3>$numa = 3e2 </h3>';
-            echo " do tipo "; $numa = 3e2; var_dump($numa);
-            echo '<hr>';
+        echo '<h3>$numa = 3e2 </h3>';
+        echo " do tipo ";
+        $numa = 3e2;
+        var_dump($numa);
+        echo '<hr>';
 
-            echo '<h3>$numa = (int) 3e2 coerção</h3>';
-            echo " do tipo "; $numa = (int) 3e2; var_dump($numa);
-            echo '<hr>';
+        echo '<h3>$numa = (int) 3e2 coerção</h3>';
+        echo " do tipo ";
+        $numa = (int) 3e2;
+        var_dump($numa);
+        echo '<hr>';
 
-            echo '<h3>$numb = (int) "750" coerção</h3>';
-            echo " do tipo "; $numb = (int) "750"; var_dump($numb);
-            echo '<hr>';
-            echo '<span>0x = HEXADECIMAL 0b = BINÁRIO 0 = OCTAL</span>';
-            // 0x indica que o número está em base 16 (hexadecimal)
-            // 1A é o valor hexadecimal
-            // 3e2 ou 3x10 ^2 ou 3x100 = 300
-           
-        ?>   
+        echo '<h3>$numb = (int) "750" coerção</h3>';
+        echo " do tipo ";
+        $numb = (int) "750";
+        var_dump($numb);
+        echo '<hr>';
+        
+        echo '<span>0x = HEXADECIMAL 0b = BINÁRIO 0 = OCTAL</span>';
+        // 0x indica que o número está em base 16 (hexadecimal)
+        // 1A é o valor hexadecimal
+        // 3e2 ou 3x10 ^2 ou 3x100 = 300
+
+        ?>
     </div>
     <div id="compostos" class="tab">
         <h2>Tipos compostos (não primitivos, mas importantes)</h2>
@@ -113,7 +128,7 @@
 
         <hr>
 
-        
+
     </div>
     <div id="especiais" class="tab">
         <?php
@@ -150,6 +165,15 @@
         ?>
         <pre id="codigo"><?php echo htmlspecialchars($codigo3); ?></pre>
         <button onclick="copiar()">Copiar código</button>
+
+
+        <?php
+            echo "<h3>Null </h3>";
+            echo '<h3>null - <code>$usuario = null </code></h3>';
+            echo "do tipo ";
+            $usuario = null;
+            var_dump($usuario);
+        ?>
     </div>
 
 
